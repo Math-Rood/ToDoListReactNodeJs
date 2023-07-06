f20f94613e539bc564ea6ca86182e224242ae5c7
Matheus William Pereira Rodrigues <mwpr.lic16@uea.edu.br>
2023-07-06 13:18:28 -0300

    Add TODO Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import './App.css';
-import FirstComponent from './components/FirstComponent';
+import "./App.css"
+import { useState, useEffect } from "react";
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs';
 
+const API = "http://localhost:5000";
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitle("");
+  };
+
   return (
     <div className="App">
-      <h2>Olá React!</h2>
-      <FirstComponent/>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <p>Create a new todo:</p>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">what you will todo?</label>
+            <p></p>
+            <input
+                type="text" 
+                name="tittle" 
+                placeholder="Todo Title" 
+                onChange={(e) => setTitle(e.target.value)}
+                value={title || ""}
+                required
+            />
+          </div>
+          <p></p>
+          <input type="submit" value="Send"/>
+        </form>
+      </div>
+      <div className="list-todo">
+        <h2>Lists</h2>
+        {todos.length === 0 && <p>Haven't to do!</p>}
+      </div>
     </div>
   );
 }
